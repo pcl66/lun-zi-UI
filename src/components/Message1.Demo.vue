d2e86193dd71338141ea68364fa3f97eebf15b8a
mac <mac@192.168.0.105>
2023-01-01 18:49:51 +0800

    feat: 修改message组件的demo

--- src/components/Message1.Demo.vue
+++ src/components/Message1.Demo.vue
@@ -2,16 +2,30 @@
 import {Message} from '../lib/Message/index';
 import Button from '../lib/Button.vue';
 
-const hClick = () => {
+const hClick1 = () => {
   Message({
     type: "success",
     text: "成功"
   })
 }
+const hClick2 = () => {
+  Message({
+    type: "warning",
+    text: "警告"
+  })
+}
+const hClick3 = () => {
+  Message({
+    type: "error",
+    text: "错误"
+  })
+}
 </script>
 
 <template>
-<Button type="danger" @click="hClick">显示成功提示消息</Button>
+<Button type="primary" @click="hClick1">显示成功提示消息</Button>
+<Button type="warning" @click="hClick2">显示警告提示消息</Button>
+<Button type="danger" @click="hClick3">显示失败提示消息</Button>
 </template>
 
 <style>
